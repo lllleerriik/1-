732c4b314353561182e06fc0b54b55d2ca0ea0e7
nikitafront <nikitapopov1295@gmail.com>
2023-05-05 21:04:47 +0500

    .
    Signed-off-by: nikitafront <nikitapopov1295@gmail.com>

--- app/src/main/java/com/webiki/bucketlist/fragments/motivation/MotivationFragment.kt
+++ app/src/main/java/com/webiki/bucketlist/fragments/motivation/MotivationFragment.kt
@@ -1,10 +1,17 @@
 package com.webiki.bucketlist.fragments.motivation
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.MediaController
 import android.widget.TextView
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.webiki.bucketlist.databinding.FragmentMotivationBinding
@@ -29,6 +36,27 @@ class MotivationFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
+        val videoView: WebView = binding.videoView
+        val videoURL = "https://www.youtube.com/watch?v=R6dk8FJkyMA"
+
+        videoView.settings.javaScriptEnabled = true
+        videoView.webViewClient  = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                url: String?
+            ): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        videoView.loadData("<html><body><iframe width=\\\"320\\\" height=\\\"315\\\"\" +\n" +
+                "            \" src=\\\"https://www.youtube.com/embed/watch?v=5kbwx78zdag\\\" frameborder=\\\"0\\\" \" +\n" +
+                "            \"allowfullscreen></iframe></body></html>",
+        "text/html",
+        "utf-8")
+
         motivationViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
